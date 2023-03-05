86b0588dab33fb47f40354845d346005a13c856c
Park Jin <tstunas@gmail.com>
2023-03-05 14:09:27 +0900

    feat: 443

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: readFileSync('/etc/letsencrypt/archive/www.sparta-tstunas.shop/privkey1.pem'),
+      cert: readFileSync('/etc/letsencrypt/archive/www.sparta-tstunas.shop/cert1.pem'), 
+    },
     cors: {
       origin: '*',
       credentials: true,
     },
   });
-  await app.listen(80);
+  await app.listen(443);
 }
 bootstrap();
